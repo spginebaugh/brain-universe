77e16a3452d3d0667f60e93c6959c3bf57ab1a8b
Scott Ginebaugh <spginebaugh@gmail.com>
2025-03-15 13:40:56 -0400

    removed unnecesary code

--- src/app/research/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { ProtectedRoute } from '@/features/auth';
-
-export default function ResearchLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <ProtectedRoute>{children}</ProtectedRoute>;
-} 
